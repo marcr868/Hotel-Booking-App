07a28c6c999bed29897f878cdb1f52a0356751f5
marc868 <mramberan@yahoo.com>
2022-07-12 21:50:01 -0400

    added FeaturedItem and other UI updates

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,12 +4,16 @@ import "./home.css";
 //component imports
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Header></Header>
+      <div className="homeContainer">
+        <Featured></Featured>
+      </div>
     </div>
   );
 };
